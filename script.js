b1fe308dd11b7e5c9f67071de79a51a14b9bfbad
Malva37 <irynapysanko@gmail.com>
2022-08-22 16:57:48 +0300

    implement function to not dublicate pieces in one space

--- script.js
+++ script.js
@@ -5,18 +5,22 @@ $(document).ready(() => {
     let top = 0;
 
     for (let i = 0; i < rows; i++) {
-
         top -= 100;
         let left = 0;
-
         for (let j = 0; j < colomns; j++) {
             let piece = document.createElement('div');
+            let pieceInFinalPicture = document.createElement('div');
+            $(pieceInFinalPicture).addClass('pieceInFinalPicture');
             $(piece).addClass('piece');
             left -= 100;
-            $(piece).css({
+            $(piece).addClass('piece')
+                    .css({
                 'background-position': left + "px " + top + "px"
+                //,
+                // position:'absolute'
             })
-            $('.puzzle').append(piece)
+            $('.puzzle').append(piece);
+            $('.picture').append(pieceInFinalPicture);
         }
     }
     let pieces = $('.piece');
@@ -29,17 +33,60 @@ $(document).ready(() => {
     }
     // return pieces;
 
-
     $('.piece').draggable({
+        revert:'invalid',
         grid: [100, 100],
         drag: (() => {
-
             $(this).css('z-index', 2);
-        })
+        }),
+        start:()=>{
+            console.log($(this))
+
+            if($(this).hasClass('droppedPiece')){
+                debugger
+                $(this).parent().removeClass('piecePresent');
+            console.log(this)
+                
+                $(this).removeClass('droppedPiece');
+
+
+            }
+        }
     })
 
-    $('.picture').droppable({
-        accept: ".piece"
-    });
+    // $('.picture').droppable({
+    //     accept: ".piece"
+    // });
+    $('.pieceInFinalPicture').droppable({
+        // classes:{
+        //     'ui-droppable':'highlight'
+        // },
+hoverClass:'ui-state-highlight',
+accept:function(){
+    return !$(this).hasClass('piecePresent')
+},
+        drop: function(event, ui){
+            console.log(event);
+            console.log(ui);
+            let draggableElement = ui.draggable;
+            let droppedOn = $(this);
+            droppedOn.addClass('piecePresent');
+            $(draggableElement).addClass('droppedPiece')
+                               .appendTo(droppedOn)
+                               .css({
+                top:0,
+                left:0,
+                position:'relative',
+                width:'100px',
+                height:'100px',
+                zIndex:1
+            })
+
+        }
+    })
+
+
+
+    
 
 })
